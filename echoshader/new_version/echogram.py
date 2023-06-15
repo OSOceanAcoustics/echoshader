f9ae76cfdc5a1ed55c0eed76cb6a5a6d6746d4ed
Dingrui Lei <56751303+ldr426@users.noreply.github.com>
2023-06-15 11:51:05 -0700

    Refactor echogram and associated components using accessor (#93)
    * Change importing format to comply with develop mode
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Solve that curtain could not dynamically render
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Select box correctly when not inverting y-axis
    
    * local testing
    
    * y-axis testing
    
    * Y-axis testing
    
    * add options for echogram
    
    * del testing files
    
    * add options
    
    * set y-axis inversion
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add testing files
    
    * rename testing file
    
    * a
    
    * a
    
    * lasso testing
    
    * add for testing
    
    * 06/06/2023 - testing
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add trigram
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Delete echoshader/new_version directory
    
    * Add testing files
    
    * rename testing file
    
    * a
    
    * a
    
    * lasso testing
    
    * add for testing
    
    * Add trigram
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * ADD testing file
    
    * del old one
    
    * add test.ipynb
    
    * change "= None" to "is None"
    
    * little fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add comments
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Sync
    
    * Add docstring and customizing colormap
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update echoshader/new_version/echogram.py
    
    Co-authored-by: Wu-Jung Lee <leewujung@gmail.com>
    
    * Update echoshader/new_version/echogram.py
    
    Co-authored-by: Wu-Jung Lee <leewujung@gmail.com>
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Solve Undefined Name
    
    * Use Literal to solve undefined issue
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Solve 'line too long' issues
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Solve 'line too long' issue
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Solve "compare False should using is not"
    
    * Delete testing files
    
    * delete testing toy dataset
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Wu-Jung Lee <leewujung@gmail.com>

--- echoshader/new_version/echogram.py
+++ echoshader/new_version/echogram.py
@@ -1,14 +1,26 @@
-from typing import Dict, Union
+from typing import Dict, List, Literal, Union
 
 import holoviews
 import panel
 import param
 import xarray
+from get_rgb import convert_to_color
+from get_string_list import convert_string_to_list
 
 
 @xarray.register_dataset_accessor("eshader")
 class Echogram(param.Parameterized):
+    """
+    A class for creating echogram visualizations from MVBS datasets.
+    """
+
     def __init__(self, MVBS_ds):
+        """
+        Initialize the Echogram object.
+
+        Args:
+            MVBS_ds (xarray.Dataset): The MVBS dataset.
+        """
         super().__init__()
 
         self.MVBS_ds = MVBS_ds
@@ -17,35 +29,77 @@ class Echogram(param.Parameterized):
             "Image": {
                 "cmap": "jet",
                 "colorbar": True,
-                "cmap": "jet",
                 "tools": ["box_select", "lasso_select", "hover"],
                 "invert_yaxis": False,
                 "width": 600,
-            }
+            },
+            "RGB": {"tools": ["hover"], "invert_yaxis": False, "width": 600},
         }
 
-    def echogram(
+        self._init_widget()
+
+    def _init_widget(self):
+        """
+        Initialize the interactive widgets for controlling the echogram visualization.
+        """
+        self.channel_select = panel.widgets.Select(
+            name="Channel Select", options=self.MVBS_ds.channel.values.tolist()
+        )
+
+        self.Sv_range_slider = panel.widgets.EditableRangeSlider(
+            name="Sv Range Slider",
+            start=self.MVBS_ds.Sv.actual_range[0],
+            end=self.MVBS_ds.Sv.actual_range[-1],
+            value=(self.MVBS_ds.Sv.actual_range[0], self.MVBS_ds.Sv.actual_range[-1]),
+            step=0.01,
+        )
+
+        self.color_map = panel.widgets.TextInput(
+            name="Color Map", value="jet", placeholder="jet"
+        )
+
+    def echogram_multiple_frequency(
         self,
-        cmap: str = "jet",
-        layout: Union[
-            str("single_frequency"), str("multiple_frequency"), str("composite")
-        ] = "single_frequency",
+        cmap: Union[str, List[str]] = "jet",
         vmin: float = None,
         vmax: float = None,
-        rgb_map: Dict[str, str] = None,
+        layout: Literal[
+            "single_frequency", "multiple_frequency", "composite"
+        ] = "single_frequency",
+        rgb_map: Dict[str, str] = {},
         *args,
         **kwargs
     ):
-        if vmin == None:
+        """
+        Generate general echogram visualizations without specified channel and control widgets.
+
+        Args:
+            cmap (str or list, optional): The colormap(s) to use.
+            Input list like ['#0000ff'] to customize. Defaults to 'jet'.
+            vmin (float, optional): The minimum value for the color range. Defaults to None.
+            vmax (float, optional): The maximum value for the color range. Defaults to None.
+            layout (str, optional): The layout of the echogram visualizations.
+                Must be one of 'single_frequency', 'multiple_frequency', or 'composite'.
+                Defaults to 'single_frequency'.
+            rgb_map (Dict[str, str], optional): The mapping of channels to RGB color channels
+                for composite visualization. Defaults to an empty dictionary.
+            *args: Additional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            panel.Tabs or panel.Column or holoviews.RGB: The generated visualizations.
+        """
+        self.gram_opts["Image"]["cmap"] = cmap
+
+        if vmin is None:
             vmin = self.MVBS_ds.Sv.actual_range[0]
 
-        if vmax == None:
+        if vmax is None:
             vmax = self.MVBS_ds.Sv.actual_range[-1]
 
         self.gram_opts["Image"]["clim"] = (vmin, vmax)
-        self.gram_opts["Image"]["cmap"] = cmap
 
-        if layout == "single_frequency":
+        if layout == str("single_frequency"):
             tabs = panel.Tabs()
 
             for index, channel in enumerate(self.MVBS_ds.channel.values):
@@ -61,7 +115,7 @@ class Echogram(param.Parameterized):
 
             return tabs
 
-        elif layout == "multiple_frequency":
+        elif layout == str("multiple_frequency"):
             col = panel.Column()
 
             for index, channel in enumerate(self.MVBS_ds.channel.values):
@@ -78,6 +132,88 @@ class Echogram(param.Parameterized):
 
             return col
 
-        elif layout == "composite":
-            if rgb_map == None:
-                pass
+        elif layout == str("composite"):
+            if rgb_map == {}:
+                rgb_map[self.MVBS_ds.channel.values[0]] = "R"
+                rgb_map[self.MVBS_ds.channel.values[1]] = "G"
+                rgb_map[self.MVBS_ds.channel.values[2]] = "B"
+
+            rgb_ch = {"R": None, "G": None, "B": None}
+
+            for ch, color in rgb_map.items():
+                rgb_ch[color] = convert_to_color(
+                    self.MVBS_ds, channel_sel=ch, th_bottom=vmin, th_top=vmax
+                )
+
+            rgb = holoviews.RGB(
+                (
+                    self.MVBS_ds.ping_time.data,
+                    self.MVBS_ds.echo_range.data,
+                    rgb_ch["R"],
+                    rgb_ch["G"],
+                    rgb_ch["B"],
+                )
+            ).opts(self.gram_opts)
+
+            return rgb
+
+    @param.depends("channel_select.value", "Sv_range_slider.value", "color_map.value")
+    def echogram_single_frequency(
+        self,
+        channel: str = None,
+        cmap: Union[str, List[str]] = None,
+        vmin: float = None,
+        vmax: float = None,
+        *args,
+        **kwargs
+    ):
+        """
+        Generate an echogram visualization for a specific channel and settings.
+
+        Args:
+            channel (str, optional): The channel to visualize. Defaults to None.
+            cmap (str or list, optional): The colormap(s) to use.
+            Input list like ['#0000ff'] to customize. Defaults to None.
+            vmin (float, optional): The minimum value for the color range. Defaults to None.
+            vmax (float, optional): The maximum value for the color range. Defaults to None.
+            *args: Additional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            holoviews.Image: The generated echogram visualization.
+        """
+        if channel is not None:
+            self.channel_select.value = channel
+
+        if cmap is not None:
+            self.color_map.value = cmap
+
+        list_camp = convert_string_to_list(self.color_map.value)
+        self.gram_opts["Image"]["cmap"] = (
+            list_camp if list_camp is not False else self.color_map.value
+        )
+
+        if vmin is not None and vmax is not None:
+            self.Sv_range_slider.start = vmin
+            self.Sv_range_slider.end = vmax
+            self.Sv_range_slider.value = (vmin, vmax)
+
+        elif vmin is not None:
+            self.Sv_range_slider.start = vmin
+            old_vmax = self.Sv_range_slider.value[1]
+            self.Sv_range_slider.value = (vmin, old_vmax)
+
+        elif vmax is not None:
+            self.Sv_range_slider.end = vmax
+            old_vmin = self.Sv_range_slider.value[0]
+            self.Sv_range_slider.value = (old_vmin, old_vmax)
+
+        self.gram_opts["Image"]["clim"] = self.Sv_range_slider.value
+
+        plot = (
+            holoviews.Dataset(self.MVBS_ds.sel(channel=self.channel_select.value))
+            .to(holoviews.Image, vdims=["Sv"], kdims=["ping_time", "echo_range"])
+            .opts(self.gram_opts)
+        )
+
+        return plot
