062959966b992e544bad70495835d345f7e5ef6a
Dingrui Lei <leidingrui426@gmail.com>
2023-07-23 21:29:18 -0700

    Add core.py, echogram.py, map.py and utils.py

---
+++ echoshader/new_version_2/utils.py
@@ -0,0 +1,38 @@
+gram_opts = {
+    "Image": {
+        "cmap": "jet",
+        "colorbar": True,
+        "tools": ["box_select", "lasso_select", "hover"],
+        "invert_yaxis": False,
+        "width": 600,
+    },
+
+    "RGB": {
+        "tools": ["hover"], 
+        "invert_yaxis": False, 
+        "width": 600
+    },
+}
+
+tiles = [
+    "CartoDark",
+    "CartoEco",
+    "CartoLight",
+    "CartoMidnight",
+    "ESRI",
+    "EsriImagery",
+    "EsriNatGeo",
+    "EsriOceanBase",
+    "EsriOceanReference",
+    "EsriReference",
+    "EsriTerrain",
+    "EsriUSATopo",
+    "OSM",
+    "OpenTopoMap",
+    "StamenLabels",
+    "StamenTerrain",
+    "StamenTerrainRetina",
+    "StamenToner",
+    "StamenTonerBackground",
+    "StamenWatercolor",
+]
