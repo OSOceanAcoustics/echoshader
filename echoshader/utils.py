52c6f321f98980492a90ad81e77977d4fa85403f
Dingrui Lei <56751303+ldr426@users.noreply.github.com>
2023-08-11 02:55:13 -0700

    Final-version Refactoring  (#131)
    * Create folders for new refactoring
    
    * Add core.py, echogram.py, map.py and utils.py
    
    * Add files
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add moored point in map & config in utils
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add echogram in core.py and docstring
    
    * Lined echogram and map for 2nd version
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Solve pre-commit issue
    
    * High-order structure building
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Solve pre-commit
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add underlying code for hist and curtain
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * sovle pre-commit
    
    * Add curtain, hist and table
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix little bugs ahout curtain and hist
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add subscriber about echogram
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add curtain
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add new requirements
    
    * Add bidirectional control logic
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add reset tool
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add customized opts
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update file stucture
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add __init__.py
    
    * Add more  docstring
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add testing demo
    
    * erase pre-commit warning
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ echoshader/utils.py
@@ -0,0 +1,49 @@
+gram_opts = {
+    "Image": {
+        "cmap": "jet",
+        "colorbar": True,
+        "tools": ["box_select", "lasso_select", "hover"],
+        "invert_yaxis": False,
+        "width": 600,
+    },
+    "RGB": {
+        "tools": ["box_select", "lasso_select", "hover"],
+        "invert_yaxis": False,
+        "width": 600,
+    },
+    "Points": {"color": "blue", "tools": ["hover"], "size": 10, "width": 600},
+    "Path": {"color": "red", "tools": ["hover"], "line_width": 1, "width": 600},
+    "Tiles": {"tools": ["box_select"], "width": 600},
+    "Histogram": {"fontsize": {"legend": 5}, "width": 600},
+    "Table": {"width": 600},
+    "Bounds": {"line_width": 1, "line_color": "white"},
+}
+
+curtain_opts = {"width": 700, "height": 500}
+
+tiles = [
+    "CartoDark",
+    "CartoEco",
+    "CartoLight",
+    "CartoMidnight",
+    "ESRI",
+    "EsriImagery",
+    "EsriNatGeo",
+    "EsriOceanBase",
+    "EsriOceanReference",
+    "EsriReference",
+    "EsriTerrain",
+    "EsriUSATopo",
+    "OSM",
+    "OpenTopoMap",
+    "StamenLabels",
+    "StamenTerrain",
+    "StamenTerrainRetina",
+    "StamenToner",
+    "StamenTonerBackground",
+    "StamenWatercolor",
+]
+
+EPSG_mercator = "EPSG:3857"
+
+EPSG_coordsys = "EPSG:4326"
