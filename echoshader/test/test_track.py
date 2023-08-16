dbcc545b54f48fcf67154b376f7b7637a8b5cd60
Dingrui Lei <leidingrui426@gmail.com>
2023-08-16 00:13:33 -0700

    Add testing files

---
+++ echoshader/test/test_track.py
@@ -0,0 +1,41 @@
+import panel
+import xarray
+
+from .. import core
+
+MVBS_ds = xarray.open_mfdataset(
+    paths="./echoshader/test/concatenated_MVBS.nc",
+    data_vars="minimal",
+    coords="minimal",
+    combine="by_coords",
+)
+
+MVBS_ds.eshader.control_mode_select.value = False
+
+track_panel = panel.Row(
+    panel.Column(MVBS_ds.eshader.tile_select), MVBS_ds.eshader.track()
+)
+
+echogram_panel = panel.Row(
+    panel.Column(MVBS_ds.eshader.Sv_range_slider, MVBS_ds.eshader.colormap),
+    MVBS_ds.eshader.echogram(),
+)
+
+tricolor_echogram_panel = panel.Row(
+    panel.Column(MVBS_ds.eshader.Sv_range_slider),
+    MVBS_ds.eshader.echogram(
+        channel=[
+            "GPT  18 kHz 009072058c8d 1-1 ES18-11",
+            "GPT  38 kHz 009072058146 2-1 ES38B",
+            "GPT 120 kHz 00907205a6d0 4-1 ES120-7C",
+        ],
+        rgb_composite=True,
+    ),
+)
+
+panel.Column(
+    MVBS_ds.eshader.control_mode_select,
+    track_panel,
+    echogram_panel,
+    tricolor_echogram_panel,
+).show()
---
+++ echoshader/test/test_tricolor_echogram.py
@@ -0,0 +1,25 @@
+import panel
+import xarray
+
+from .. import core
+
+MVBS_ds = xarray.open_mfdataset(
+    paths="./echoshader/test/concatenated_MVBS.nc",
+    data_vars="minimal",
+    coords="minimal",
+    combine="by_coords",
+)
+
+tricolor_echogram_panel = panel.Row(
+    panel.Column(MVBS_ds.eshader.Sv_range_slider),
+    MVBS_ds.eshader.echogram(
+        channel=[
+            "GPT  18 kHz 009072058c8d 1-1 ES18-11",
+            "GPT  38 kHz 009072058146 2-1 ES38B",
+            "GPT 120 kHz 00907205a6d0 4-1 ES120-7C",
+        ],
+        rgb_composite=True,
+    ),
+)
+
+tricolor_echogram_panel.show()
